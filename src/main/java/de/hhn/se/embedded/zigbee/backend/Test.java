8f4da51361f691567db67a65a78bc855972a5b75
florensh <florensh@gmx.de>
2016-04-15 10:00:17 +0200

    now with files

---
+++ src/main/java/de/hhn/se/embedded/zigbee/backend/Test.java
@@ -0,0 +1,18 @@
+package de.hhn.se.embedded.zigbee.backend;
+
+public class Test {
+	private String status;
+	
+	public Test(String status){
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
