463aae2b4ed4a6835fb90a377c70695d2cadc1c7
florensh <florensh@gmx.de>
2016-05-19 18:22:10 +0200

    minor changes

--- src/main/java/de/hhn/se/embedded/zigbee/backend/security/UserController.java
+++ src/main/java/de/hhn/se/embedded/zigbee/backend/security/UserController.java
@@ -2,15 +2,12 @@ package de.hhn.se.embedded.zigbee.backend.security;
 
 import java.util.List;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
